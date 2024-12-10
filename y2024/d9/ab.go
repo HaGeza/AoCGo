b41a6603c82d69811e38cd373460562dbae519b5
HaGeza <pelokbalazs@gmail.com>
2024-12-10 01:25:32 +0100

    day 9 part 2 started

--- y2024/d9/ab.go
+++ y2024/d9/ab.go
@@ -6,8 +6,22 @@ import (
 	"os"
 )
 
+type Node struct {
+	size byte
+	free bool
+	id   int
+	pos  int
+	prev *Node
+	next *Node
+}
+
+type DLL struct {
+	head *Node
+	tail *Node
+}
+
 func main() {
-	file, err := os.Open("data/d9/a.txt")
+	file, err := os.Open("data/d9/test.txt")
 	if err != nil {
 		fmt.Println("Error opening file:", err)
 		return
@@ -62,32 +76,99 @@ outer:
 	fmt.Println("Partial move sum:", sum)
 
 	// Part 2
-	sum, pos = 0, 0
-
 	copy(data, dataOriginal)
 	copy(free, freeOriginal)
 
-	for i := 0; i < len(data); i++ {
-		d := int(data[i])
-		sum += i * (d*pos + d*(d-1)/2)
-		pos += int(dataOriginal[i])
+	dll := &DLL{}
+	newData := &Node{size: data[0], free: false, id: 0, pos: 0}
+	dll.head = newData
+	dll.tail = newData
 
-		if i >= len(free) {
-			continue
+	newFree := &Node{size: free[0], free: true, prev: dll.head, id: 0, pos: int(data[0])}
+	dll.tail = newFree
+	dll.head.next = newFree
+
+	for i := 1; i < len(data); i++ {
+		pos = dll.tail.pos + int(dll.tail.size)
+		newData = &Node{size: data[i], free: false, id: i, pos: pos, prev: dll.tail}
+		dll.tail.next = newData
+		dll.tail = newData
+
+		if i < len(free) {
+			pos = dll.tail.pos + int(dll.tail.size)
+			newFree = &Node{size: free[i], free: true, id: i, pos: pos, prev: dll.tail}
+			dll.tail.next = newFree
+			dll.tail = newFree
 		}
+	}
 
-		for j := len(data) - 1; free[i] > 0 && j >= 0; j-- {
-			if data[j] > 0 && data[j] <= free[i] {
-				m := int(data[j])
+	node := dll.head
+	for node != nil {
+		if node.free {
+			nodeToMove := dll.tail
+		movingLoop:
+			for nodeToMove != nil {
+				if !nodeToMove.free && nodeToMove.size <= node.size {
+					node.size -= nodeToMove.size
+					nodeToMove.free = true
+					// Merge free space after
+					next := nodeToMove.next
+					if next != nil && next.free {
+						nodeToMove.size += next.size
+						if next.next != nil {
+							next.next.prev = nodeToMove
+						} else {
+							dll.tail = nodeToMove
+						}
+						next = next.next
 
-				free[i] -= data[j]
-				data[j] = 0
+						next.prev, next.next = nil, nil
+					}
+					// Merge free space before
+					prev := nodeToMove.prev
+					if prev != nil && prev.free {
+						prev.size += nodeToMove.size
+						prev.next = nodeToMove.next
+						if nodeToMove.next != nil {
+							nodeToMove.next.prev = prev
+						} else {
+							dll.tail = prev
+						}
 
-				sum += j * (m*pos + m*(m-1)/2)
-				pos += m
+						nodeToMove.prev, nodeToMove.next = nil, nil
+					}
+					// Add new data
+					newData = &Node{
+						size: nodeToMove.size,
+						free: false,
+						id:   nodeToMove.id,
+						pos:  node.pos,
+						prev: node.prev,
+						next: node,
+					}
+					if node.prev != nil {
+						node.prev.next = newData
+					}
+					node.prev = newData
+					// Remove free space if filled, otherwise update position
+					if node.size == 0 {
+						node.prev.next = node.next
+						node.prev, node.next = nil, nil
+						break movingLoop
+					} else {
+						node.pos += int(nodeToMove.size)
+					}
+				}
+				nodeToMove = nodeToMove.prev
 			}
 		}
-		pos += int(free[i])
+		node = node.next
 	}
-	fmt.Println("Whole move sum:", sum)
+
+	sum = 0
+	node = dll.head
+	for node != nil {
+		fmt.Println(node)
+		node = node.next
+	}
 }
